e0bdf20b2d06e032ae01e5bc8892176ff8227711
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2023-10-13 14:58:21 +0200

    .

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -14,12 +14,55 @@ const WeatherApp = () => {
 
   let api_key = "ac4c316660f753996f55df564f00f2e9"
 
-  const search = ()=>{
+ /* const search = async ()=>{
       const element = document.getElementsByClassName("cityInput")
       if(element[0].value === ""){
         return 0;
       }
-  }
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+
+     let res = await fetch(url);
+     let data = res.json()
+     const humidity = document.getElementsByClassName("humidity-percent")
+     const wind = document.getElementsByClassName("wind-rate")
+     const temp = document.getElementsByClassName("weather-temp")
+     const location = document.getElementsByClassName("weather-location")
+
+     humidity[0].innerHTML = data.main.humidity;
+     wind[0].innerHTML = data.wind.speed;
+     temp[0].innerHTML = data.main.temp;
+     location[0].innerHTML = data.name;
+
+  }*/
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+  
+    try {
+      const res = await fetch(url);
+      if (res.ok) {
+        const data = await res.json();
+        const humidity = document.getElementsByClassName("humidity-percent");
+        const wind = document.getElementsByClassName("wind-rate");
+        const temp = document.getElementsByClassName("weather-temp");
+        const location = document.getElementsByClassName("weather-location");
+  
+        humidity[0].innerHTML = data.main.humidity;
+        wind[0].innerHTML = data.wind.speed;
+        temp[0].innerHTML = data.main.temp;
+        location[0].innerHTML = data.name;
+      } else {
+        console.error("Erreur lors de la récupération des données météo.");
+      }
+    } catch (error) {
+      console.error("Une erreur est survenue lors de la récupération des données : " + error);
+    }
+  };
+  
 
   return (
     <div>
@@ -48,7 +91,7 @@ const WeatherApp = () => {
         <div className="element">
           <img src={wind_icon} className='icon' alt="" />
           <div className="data">
-            <div className="humidity-percent">
+            <div className="wind-rate">
               18km/h
             </div>
             <div className="text">Wind Speed</div>
